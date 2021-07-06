5cdce8f21d3a1bdedf708f161e64bd9cefe838e3
lilyann95 <nabawesil95@gmail.com>
2021-07-06 13:14:26 +0400

    week1 exercises

---
+++ week1 exercises/js/script.js
@@ -0,0 +1,33 @@
+// var x = 10;
+// if ( (null) || (console.log("Hello")) || x > 5 ) {
+//   console.log("Hello");
+// }
+
+// var a = 7;
+// var a = {x: 7};
+// var b = a;
+// b.x = 5;
+// console.log(b);
+// console.log(a);
+
+// var x = 5;
+// var y = x;
+// x = 10;
+// console.log(y);
+// function Dog(name) {
+//   this.name = name;
+// }
+
+// Dog.prototype.bark = function () {
+//   console.log(this.name + " likes barking! Bark!");
+// }
+
+// var max = new Dog("Max", "Buddy");
+// max.bark();
+
+// var counter = 0;
+// var myArray = ["Yaakov", 2, {handle: "yaakovchaikin"}];
+// for (var i = 0; i < myArray.length; i++) {
+//   counter++;
+// }
+// console.log(counter);
